abf74e84b6b62d1d5eb2f4b6f0756387b2a6491d
tingirifistik <51286195+tingirifistik@users.noreply.github.com>
2023-07-29 13:32:26 +0300

    Update HourBooster.py

--- bot.py
+++ bot.py
@@ -1,144 +1,118 @@
 from steam.client import SteamClient
-import requests
-# from threading import Thread
-from os import environ
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, CallbackContext, ConversationHandler
 
-token = environ.get("TOKEN")
+
+TOKEN = ""
+USERNAME, PASSWORD, GAME = 0, 1, 2
+GUARD, MAIL = 0, 1
+
 client = SteamClient()
 
-def getUpdate():
-    url = 'https://api.telegram.org/bot{}/getUpdates'.format(token)
-    r = requests.get(url)
-    x = 0
-    while 1 :
-        try:
-            r.json()["result"][x]
-            x+=1
-        except IndexError:
-            return (r.json()["result"][x-1]["message"]["chat"]["id"]), r.json()["result"][x-1]["message"]["text"], (r.json()["result"][x-1]["message"]["date"])
 
-def sendMessage(text, id):
-    requests.get(f"https://api.telegram.org/bot{token}/sendMessage?chat_id={id}&text=" + text, timeout=3)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f"Hello,\nThis bot increases the hours played of games on Steam\.\nType *_/help_* to learn how to use it\.\n\n[_Source Code_](https://github.com/tingirifistik/Steam-HourBooster)\n[_Twitter_](https://twitter.com/_tingirifistik)", parse_mode='MarkdownV2')
+
+async def help_command(update: Update, conext: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text(f"*_/config_* \-\-\> Saves your Steam username, password, and the games you want to increase hours played\.\n\n*_/run_* \-\-\> Begins the process of increasing hours played\.", parse_mode='MarkdownV2')
     
-# def boost():
-#     client.games_played(oyunID)
-#     client.run_forever()
+async def what(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    await update.message.reply_text("I couldn't understand what you wrote.")
 
-date_list = []
-while 1:
-    try:
-        id, text, date = getUpdate()
+
+async def run(update: Update, context: CallbackContext) -> None:
+    with open("config", "r", encoding="UTF-8") as f:
+        r = f.read()
+        global username, password, games
+        username, password, games = r.split(":")[0], r.split(":")[1].split("\n")[0], r.split("\n")[1].split(",")
+    account_login = client.login(username=username, password=password)
+    if str(account_login) == "85":
+        await update.message.reply_text("Please enter your Steam Guard code.")
+        return GUARD
+    elif str(account_login) == "84":
+        await update.message.reply_text("Please enter the Steam Guard code sent to your email.")
+        return MAIL
+    elif str(account_login) == "1":
+        await update.message.reply_text("You have successfully logged in. Games are running.")
+        client.games_played(games)
+        client.run_forever()
+    else:
+        await update.message.reply_text(f"Error Code: {str(account_login)}")
+        return ConversationHandler.END
     
-        if text == "/config" and date not in date_list:
-            date_list.append(date)
-            sendMessage("Please enter your username and password with a colon between them.\n\nExample: kralali01:31mizAh31", id)
-            while 1:
-                id, text, date = getUpdate()
-                gecici = text
-                if ":" in text:
-                    sendMessage(f"{text}  -->  Is the username and password correct? (If yes, type 'yes', if not, press 'enter')", id)
-                    while 1:
-                        id, text, date = getUpdate()
-                        if text == "yes" or text == "Yes":
-                            login = gecici
-                            sendMessage("Please enter the ID's of the games separated by commas.\n\nExample: 730,302080,359550", id)
-                            while 1:
-                                id, text, date = getUpdate()
-                                if "," in text:
-                                    verified = []
-                                    for i in text.split(","):
-                                        verify = requests.get(f"https://store.steampowered.com/api/appdetails/?appids={i}&filters=basic").json()
-                                        if verify == "null":
-                                            pass
-                                        elif verify[str(i)]["success"] == False:
-                                            pass
-                                        else:
-                                            game_name = str(verify[str(i)]["data"]["name"])
-                                            verified.append(int(i))
-                                    if verified == []:
-                                        sendMessage("The game IDs are incorrect. Please try again.", id)
-                                        break
-                                    with open("config", "w", encoding="UTF-8") as file:
-                                        file.write(f"{login}\n{str(verified)}")
-                                        date_list.append(date)
-                                        sendMessage("Your settings have been saved successfully.", id)
-                                    break
-                            break
-                        elif text!= gecici and text != "yes" and text != "Yes":
-                            date_list.append(date)
-                            sendMessage("Settings were not saved.", id)
-                            break    
-                    break
-                elif text != "/config" and ":" not in text and date not in date_list:
-                    date_list.append(date)
-                    sendMessage("Please enter your username and password with a colon between them.\n\nExample: kralali01:31mizAh31", id)
-                    break
-                
-        elif text == "/run" and date not in date_list:    
-            date_list.append(date)
-            try:
-                with open("config", "r", encoding="UTF-8") as file:
-                    login = file.readline().split("\n")[0]
-                    oyunID = file.readline().strip('][').split(', ')
-                username = login.split(":")[0]
-                password = login.split(":")[1]
-                account_login = client.login(username=username, password=password)
-                if str(account_login) == "EResult.AccountLoginDeniedNeedTwoFactor":
-                    sendMessage("Please enter your Steam Guard code.", id)
-                    while 1:
-                        id, text, date = getUpdate()
-                        if text != "/run":
-                            date_list.append(date)
-                            account_login = client.login(username=username, password=password, two_factor_code=text)
-                            if str(account_login) == "EResult.InvalidPassword":
-                                sendMessage("Wrong password!\nEdit your password with the '/config' command." , id)
-                                break
-                            elif str(account_login) == "EResult.OK":
-                                sendMessage("You have successfully logged in. Games are running.", id)
-                                client.games_played(oyunID)
-                                client.run_forever()
-                                break
-                            elif str(account_login) == "EResult.TwoFactorCodeMismatch":
-                                sendMessage('Incorrect guard code!\nPlease try again.', id)
-                                break
-                elif str(account_login) == "EResult.AccountLogonDenied":
-                    sendMessage("Please enter the Steam Guard code sent to your email.", id)
-                    while 1:
-                        id, text, date = getUpdate()
-                        if text != "/run":
-                            date_list.append(date)
-                            account_login = client.login(username=username, password=password, auth_code=text)
-                            if str(account_login) == "EResult.InvalidPassword":
-                                sendMessage("Wrong password!\nEdit your password with the '/config' command." , id)
-                                break
-                            elif str(account_login) == "EResult.OK":
-                                sendMessage("You have successfully logged in. Games are running..", id)
-                                client.games_played(oyunID)
-                                client.run_forever()
-                                break
-                            elif str(account_login) == "EResult.InvalidLoginAuthCode":
-                                sendMessage('Incorrect guard code!\nPlease try again.', id)
-                                break
-                elif str(account_login) == "EResult.InvalidPassword":
-                    sendMessage("Wrong password!\nEdit your password with the '/config' command." , id)
-                elif str(account_login) == "EResult.OK":
-                    client.games_played(oyunID)
-                    client.run_forever()
-                else:
-                    sendMessage("Hata: " + str(account_login), id)        
-            except FileNotFoundError:
-                sendMessage("Enter your username and password by using the '/config' command.", id)  
+async def guard(update: Update, context: CallbackContext) -> None:
+    guard_code = update.message.text
+    account_login = client.login(username=username, password=password, two_factor_code=guard_code)
+    if str(account_login) == "1":
+        await update.message.reply_text("You have successfully logged in. Games are running.")
+        client.games_played(games)
+        client.run_forever()
+    else:
+        await update.message.reply_text("The guard code is wrong!")
+        return ConversationHandler.END
 
-        elif text == "/start" and date not in date_list:
-            date_list.append(date)
-            sendMessage("Hello,\nThis bot increases the hours played of games on Steam.\nType '/help' to learn how to use it.", id)
-        
-        elif text == "/help" and date not in date_list:
-            date_list.append(date)
-            sendMessage("/config --> Saves your Steam username, password, and the games you want to increase hours played.\n\n/run --> Begins the process of increasing hours played.", id)
-        
-        elif text != "/config" and text != "/run" and text != "/start" and text != "/help" and date not in date_list:
-            date_list.append(date)
-            sendMessage("I did not understand what you wrote.\n\nType '/help' to see the commands.", id)
-    except:
-        pass
+async def mail(update: Update, context: CallbackContext) -> None:
+    mail_code = update.message.text
+    account_login = client.login(username=username, password=password, auth_code=mail_code)
+    if str(account_login) == "1":
+        await update.message.reply_text("You have successfully logged in. Games are running.")
+        client.games_played(games)
+        client.run_forever()
+    else:
+        await update.message.reply_text("The verification code is wrong!")
+        return ConversationHandler.END
+
+
+async def config(update: Update, context: CallbackContext) -> int:
+    await update.message.reply_text("Enter your Steam username..")
+    return USERNAME
+
+async def get_username(update: Update, context: CallbackContext) -> int:
+    context.user_data['username'] = update.message.text
+    await update.message.reply_text("Enter your Steam password..")
+    return PASSWORD
+
+async def get_password(update: Update, context: CallbackContext) -> int:
+    context.user_data['password'] = update.message.text
+    await update.message.reply_text("Enter the game IDs, separated by commas..")
+    return GAME
+    
+async def get_games(update: Update, context: CallbackContext) -> int:
+    context.user_data['games'] = update.message.text
+    await update.message.reply_text("Your settings have been saved..")
+    with open("config", "w", encoding="utf-8") as file:
+        file.write(f"{context.user_data['username']}:{context.user_data['password']}\n{context.user_data['games']}")
+    return ConversationHandler.END
+
+
+def main() -> None:
+    application = Application.builder().token(TOKEN).build()
+
+    config_handler = ConversationHandler(
+        entry_points=[CommandHandler("config", config)],
+        states={
+            USERNAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_username)],
+            PASSWORD: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_password)],
+            GAME: [MessageHandler(filters.TEXT & ~filters.COMMAND, get_games)]
+        },
+        fallbacks=[]
+    )
+    login_handler = ConversationHandler(
+        entry_points=[CommandHandler("run", run)],
+        states={
+            GUARD: [MessageHandler(filters.TEXT & ~filters.COMMAND, guard)],
+            MAIL: [MessageHandler(filters.TEXT & ~filters.COMMAND, mail)]
+        },
+        fallbacks=[]
+    )
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(config_handler)
+    application.add_handler(login_handler)
+    application.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, what))
+
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
